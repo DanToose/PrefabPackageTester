2380d51ece6ba12037277b93c5cb1d4b2b967d1a
Dan Toose <dantoose@gmail.com>
2022-04-05 17:09:41 +1000

    Adding Collectable items for M&T
    Planks and Fire Spell collectables.
    
    Also added basic inventory counter script to player

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public int plankCount;
+    public int fireSpellCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plankCount = 0;
+        fireSpellCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
