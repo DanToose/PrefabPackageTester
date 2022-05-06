0f6bf7e8f8f1d0c92fc831f268c281ec4d9b246d
Dan Toose <dantoose@gmail.com>
2022-05-06 17:08:25 +1000

    Minor scene changes - Audio mixer added
    Check Prefabs and package building - Things are getting weird man.

--- Assets/Scripts/AudioTrigger.cs
+++ Assets/Scripts/AudioTrigger.cs
@@ -8,14 +8,20 @@ public class AudioTrigger : MonoBehaviour
     private AudioClip soundToPlay;
     public bool remoteSoundEffect; //Check this in Inspector if you want this played elsewhere
     public GameObject remoteSFXLocation;
+    private Vector3 remoteObjectLoc;
     public AudioSource sourceToPlay; // THIS NEEDS TO BE AN AUDIOSOURCE COMPONENT IN YOUR LEVEL! Maybe 'SFXSytem'
     public float volume;
+    public bool destructible;
 
     private void Start()
     {
         if (remoteSFXLocation == null)
         {
             remoteSoundEffect = false;
+        }
+        else
+        {
+            remoteObjectLoc = remoteSFXLocation.transform.position;
         }
     }
 
@@ -25,16 +31,25 @@ public class AudioTrigger : MonoBehaviour
         {
             if (remoteSoundEffect)
             {
-                //PlayClipAtPoint(soundToPlay, remoteSFXLocation.gameObject.transform.position); // THIS PLAYS IT AT THE ITEM PICKUP LOCATION
+                AudioSource.PlayClipAtPoint(soundToPlay, remoteObjectLoc); // THIS PLAYS IT AT THE ITEM PICKUP LOCATION
             }
             else
             {
-                sourceToPlay.PlayOneShot(soundToPlay, volume); //THIS PLAYS IT AT THE PLAYER LOCATION
+                PlaySoundClip();
             }
             // If this is a pickup, you'd TOTALLY also do something here that shows the player has picked it up - Maybe flag a boolean in another script, etc
-            Destroy(gameObject); //IF YOU KEEP THIS, THE ITEM WILL BE REMOVED ONCE IT IS RUN OVER!
-            // IF YOU WANT THIS TO JUST BE A SFX TRIGGER ZONE, comment out the DESTROY line above, and make the collider an invisible trigger.
+            
+            if (destructible)
+            {
+                Destroy(gameObject); 
+            }
+                  
         }
+    }
+
+    public void PlaySoundClip()
+    {
+        sourceToPlay.PlayOneShot(soundToPlay, volume); //THIS PLAYS IT AT THE PLAYER LOCATION
     }
 
 }
