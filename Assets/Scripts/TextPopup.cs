68fa19983108565415fc2233575c96b1b9c8c337
Dan Toose <dantoose@gmail.com>
2022-05-10 17:00:42 +1000

    Updated text popup system
    Now responds properly.

--- Assets/Scripts/TextPopup.cs
+++ Assets/Scripts/TextPopup.cs
@@ -28,28 +28,33 @@ public class TextPopup : MonoBehaviour
     {
         keyWasPressed = false;
         keyWasPressed = Input.GetKeyDown(keyToAdvanceText);
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player" && fieldIsActive == false)
-        {
-            textField.gameObject.SetActive(true);
-            fieldIsActive = true;
-        }
-        
+
         if (fieldIsActive == true && keyWasPressed)
         {
+            Debug.Log("TN = " + textNumber);
+            
             if (textNumber >= popupTexts.Length)
-            {
+            {              
                 ResetTextField();
             }
             if (textNumber < popupTexts.Length)
             {
                 textField.text = popupTexts[textNumber];
                 textNumber++;
+                //keyWasPressed = false;
             }
-        }         
+        }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && fieldIsActive == false)
+        {
+            textField.gameObject.SetActive(true);
+            fieldIsActive = true;
+        }
+    }
+
 
     private void OnTriggerExit(Collider other)
     {
@@ -61,9 +66,10 @@ public class TextPopup : MonoBehaviour
 
     private void ResetTextField()
     {
+        Debug.Log("Reset Text Field called");
+        textNumber = 0;
         textField.text = initialMessage;
         textField.gameObject.SetActive(false);
         fieldIsActive = false;
-        textNumber = 0;
     }
 }
